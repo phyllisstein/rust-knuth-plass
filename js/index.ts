7d40d114db387745803b931d51b5d2b4da92b7b9
Daniel P. Shannon <daniel@try.works>
2023-12-23 14:01:26 -0500

    👷 Transpile TypeScript with Babel.

--- js/index.ts
+++ js/index.ts
@@ -1,5 +1,5 @@
 void (async () => {
-  const rust = await import('../rust/pkg')
+  const rust = await import('../rust/pkg/index.js')
   const txt = rust.break_lines('5 Reasons Drinking Milk On the Toilet Is Kind of a Game-Changer')
   console.log({ txt })
 })()
