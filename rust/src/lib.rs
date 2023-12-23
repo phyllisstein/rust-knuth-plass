a0915c18792cafa766a4b92fed51bbb9a6ebc850
Daniel P. Shannon <daniel@try.works>
2023-12-23 13:11:08 -0500

    ⚗️ Test Wasm/JS bridge with naïve hyphenation.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,26 @@
 use wasm_bindgen::prelude::*;
 
-// Import the `window.alert` function from the Web.
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
-}
+pub fn break_lines(text: &str) -> String {
+    let text_string = String::from(text);
+    let mut words = vec![];
 
-// Export a `greet` function from Rust to JavaScript, that alerts a
-// hello message.
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+    for word in text_string.split_whitespace() {
+        let word_string = String::from(word);
+        let word_length = word_string.len();
+
+        if word_length <= 3 {
+            words.push(word_string);
+            continue;
+        }
+
+        let (first, last) = word_string.split_at(word_length / 2);
+        let hyphen_word = format!("{}-{}", first, last);
+        words.push(hyphen_word);
+    }
+
+    let result = words.join(" ");
+    let result = format!("<p>{}</p>", result);
+
+    result
 }
