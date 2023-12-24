cdfea3888e32e96e52bcef4bb3ebe68f41b98a05
Daniel P. Shannon <daniel@try.works>
2023-12-24 17:14:59 -0500

    ♻️ Extract break_lines into lines module.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,26 +1,8 @@
+pub mod lines;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn break_lines(text: &str) -> String {
-    let text_string = String::from(text);
-    let mut words = vec![];
-
-    for word in text_string.split_whitespace() {
-        let word_string = String::from(word);
-        let word_length = word_string.len();
-
-        if word_length <= 3 {
-            words.push(word_string);
-            continue;
-        }
-
-        let (first, last) = word_string.split_at(word_length / 2);
-        let hyphen_word = format!("{}-{}", first, last);
-        words.push(hyphen_word);
-    }
-
-    let result = words.join(" ");
-    let result = format!("<p>{}</p>", result);
-
-    result
+    lines::break_lines::break_lines(text)
 }
