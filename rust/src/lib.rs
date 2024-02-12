e0c919ee8769f7a6ea5a6dc480f3f05b2377f3bf
Daniel P. Shannon <daniel@try.works>
2024-02-12 06:15:15 -0500

    ⚗️ Try to make HTTP request from wasm, fail.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,23 @@
 pub mod lines;
 
+use error_chain::error_chain;
+use std::io::Read;
 use lines::graf::Graf;
 use wasm_bindgen::prelude::*;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
 
 #[wasm_bindgen]
 pub fn break_lines(lines: &str) -> String {
     let mut g = Graf::new(lines.to_string());
+    let hyphens = g.get_hyphens();
 
-    g.get_hyphens()
+    let res = reqwest::blocking::get("http://httpbin.org/get").unwrap();
+
+    res.text().unwrap()
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,7 +1,15 @@
 pub mod lines;
 
+use error_chain::error_chain;
 use lines::graf::Graf;
 use std::env;
+
+error_chain! {
+    foreign_links {
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
 
 fn main() {
     let text = env::args().nth(1).unwrap_or(
@@ -13,4 +21,9 @@ fn main() {
     let mut graf = Graf::new(String::from(&text));
 
     println!("{:?}", graf.get_hyphens());
+
+    let res = reqwest::blocking::get("http://httpbin.org/get").unwrap();
+
+    let body = res.text().unwrap();
+    println!("Body: {}", body);
 }
