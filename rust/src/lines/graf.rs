54fd51b52b9c9450d315b252793b3c395417272e
Daniel P. Shannon <daniel@try.works>
2024-07-27 12:18:57 -0400

    🚧 WIP.

--- rust/src/lines/graf.rs
+++ rust/src/lines/graf.rs
@@ -1,10 +1,12 @@
 use crate::lines::nodes::*;
+use std::collections::HashMap;
+
 
 #[derive(Debug, Copy, Clone)]
 pub struct Breakpoint {
     active: bool,
     position: usize,
-    total_demerits: u32,
+    // total_demerits: u32,
     total_shrinkability: u32,
     total_stretchability: u32,
     total_width: u32,
@@ -12,9 +14,9 @@ pub struct Breakpoint {
 
 #[derive(Debug, Clone)]
 pub struct Graf {
-    boxes: Vec<BoxNode>,
-    breakpoints: Vec<Breakpoint>,
-    glue: Vec<GlueNode>,
+    boxes: HashMap<usize, BoxNode>,
+    breakpoints: HashMap<usize, Breakpoint>,
+    glue: HashMap<usize, GlueNode>,
     plain_text: String,
 }
 
@@ -24,35 +26,55 @@ impl Graf {
     pub fn new(plain_text: String) -> Graf {
         Graf {
             plain_text,
-            boxes: vec![],
-            glue: vec![],
-            breakpoints: vec![],
+            boxes: HashMap::new(),
+            glue: HashMap::new(),
+            breakpoints: HashMap::new(),
         }
     }
 
     fn parse_nodes(&mut self) {
-        self.breakpoints.clear();
-        self.breakpoints.push(Breakpoint {
-            active: true,
-            position: 0,
-            total_demerits: 0,
-            total_shrinkability: 0,
-            total_stretchability: 0,
-            total_width: 0,
-        });
-
-        for (position, grapheme) in self.plain_text.chars().into_iter().enumerate() {
+        for (position, grapheme) in self.plain_text.chars().enumerate() {
             if let Some(node) = BoxNode::from_char(grapheme, position) {
-                self.boxes.push(node);
+                self.boxes.insert(position, node);
             }
 
             if let Some(node) = GlueNode::from_char(grapheme, position) {
-                self.glue.push(node);
+                self.glue.insert(position, node);
             }
         }
+    }
 
-        println!("{:?}", self.boxes);
-        println!("{:?}", self.glue);
+    fn compute_breakpoints(&mut self) {
+        let mut active_breakpoint = Breakpoint {
+            active: true,
+            position: 0,
+            // total_demerits: 0,
+            total_shrinkability: 0,
+            total_stretchability: 0,
+            total_width: 0,
+        };
+
+        for (&position, &glue) in self.glue.iter() {
+            let mut new_breakpoint = active_breakpoint;
+
+            // new_breakpoint.total_demerits += glue.total_demerits;
+            new_breakpoint.total_shrinkability += glue.shrinkability;
+            new_breakpoint.total_stretchability += glue.stretchability;
+            new_breakpoint.total_width += glue.width;
+
+            if new_breakpoint.total_width > Self::TARGET_LINE_LENGTH as u32 {
+                new_breakpoint.active = false;
+                self.breakpoints.insert(position, new_breakpoint);
+                active_breakpoint = Breakpoint {
+                    active: true,
+                    position,
+                    // total_demerits: 0,
+                    total_shrinkability: 0,
+                    total_stretchability: 0,
+                    total_width: 0,
+                };
+            }
+        }
     }
 
     pub fn get_hyphens(&mut self) -> String {
@@ -60,7 +82,7 @@ impl Graf {
 
         self.parse_nodes();
 
-        for (position, breakpoint) in self.breakpoints.iter().enumerate() {
+        for (&position, breakpoint) in self.breakpoints.iter() {
             let new_position = breakpoint.position + (position * 5); // 5 is the length of `&shy;`
             hyphens.insert_str(new_position, "&shy;");
         }
